4f46b07b7dbd4062c6e9354f03834f5ffa6aead7
xRazerSwiftx <18rwalsh@mlmc.vic.edu.au>
2017-11-27 12:07:01 +1100

    changed the code so it reads from the file genomic_dna.txt

--- Splitting Genomic DNA.py
+++ Splitting Genomic DNA.py
@@ -1,15 +1,23 @@
-dnaSeq = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
+#opening and preparing the file to be read and manipulated
+dnaSeq = open('/Users/xRazerSwiftx/Desktop/genomic_dna.txt')
+myDna = dnaSeq.read().strip('\n')
+
+#creating the files
 myFile1 = open('CodingDNA.txt', 'w')
 myFile2 = open('NonCodingDNA.txt', 'w')
 
 #find exon 1
-exon1 = dnaSeq[0:63]
+exon1 = myDna[0:63]
 
 #find intron
-intron = dnaSeq[63:90]
+intron = myDna[63:90]
 
 #find exon 2
-exon2 = dnaSeq[90:]
+exon2 = myDna[90:]
 
+#writing the str to the files
 myFile1.write("Coding DNA: {0}\nCoding DNA: {1}".format(exon1, exon2))
 myFile2.write('Non Coding DNA: {0}'.format(intron))
+
+myFile1.close()
+myFile2.close()
